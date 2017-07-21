75b43103fbbe7473771353739afee63ef1926187
Lina Montes <monteslina54@gmail.com>
2017-07-21 12:28:32 -0400

    new variable

---
+++ python/grades.py
@@ -0,0 +1,46 @@
+from numpy import *
+
+# Each of our grades
+kristen_grades = [90, 85, 100, 77, 94]
+clarisse_grades = [96, 83, 89, 97, 86]
+sapna_grades = [82, 91, 94, 87, 99]
+
+# Class grade book
+grade_book = [kristen_grades, clarisse_grades, sapna_grades]
+
+'''
+This is what our grade book looks like
+[	[90, 85, 100, 77, 94]
+	[96, 83, 89, 97, 86]
+	[82, 91, 94, 87, 99] ]
+
+'''
+
+# Traverse through the grade book and print all the indivdual grades
+print("The grades are below:")
+for row in grade_book:
+	for x in row:
+		print(x)
+	print()
+
+
+# Extensions: Find the class average, median, and standard deviation
+# (For the extentions google for hints!)
+
+total_sum=0
+test=0
+
+for row in grade_book:
+	for x in row:
+		total_sum=x+total_sum
+		test=1+test
+
+
+
+print("The total sum is as follows:")
+print(total_sum)
+print()
+print("The average is as follows:")
+print(total_sum/test)
+
+# Super extra extensions: calculate the student with highest/lowest average
