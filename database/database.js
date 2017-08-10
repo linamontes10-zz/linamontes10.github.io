9288e5973d11af4fb5ff31cd1fe8e3b39ceba494
Lina Montes <monteslina54@gmail.com>
2017-08-10 15:54:44 -0400

    finals

--- database/database.js
+++ database/database.js
@@ -6,21 +6,58 @@ class TaskEntry {
 
 }
 
+var id = 0;
+
 function addTaskToTheToDoList() {
-  //Create new blog entry
   var taskText = document.getElementById("taskEntryText").value;
   var taskEntry = new TaskEntry(taskText);
-
-  //Add the new entry, name and date to the blog
   createTaskEntryElement(taskEntry);
-  //Clear the inputs
+}
 
+function deleteTask() {
+  var checks = document.getElementsByClassName("checkboxClass");
+  var tasks = document.getElementsByClassName("taskEntry");
+
+  var numChecked = 0;
+
+  for (var i = 0; i < tasks.length; i++){
+    var check = tasks[i];
+
+    var c = check.getElementsByClassName("checkboxClass")[0];
+    console.log(c)
+    if (c.checked) {
+      numChecked++;
+    }
+  }
+
+  console.log(numChecked)
+
+  while (numChecked > 0) {
+      for (var i = 0; i < tasks.length; i++){
+        var check = tasks[i].getElementsByClassName("checkboxClass");
+
+        if (check.checked) {
+          console.log("Deleting " + check);
+          document.getElementById(check.id).remove();
+          numChecked--;
+        }
+      }
+  }
 }
 
 function createTaskEntryElement(taskEntry) {  
   var taskText = taskEntry.task_text;
   var node = document.createElement("div");
+  node.id = id;
+
+  var checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.className = "checkboxClass";
+
   node.innerHTML = taskText;
   node.className = "taskEntry"; 
+
+  taskDetails.appendChild(checkbox);
   taskDetails.appendChild(node);
+  id++;
 }
